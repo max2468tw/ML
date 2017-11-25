b9af11824c6bf92dad5764dcbb5e33b5258c085e
Max_ubuntu <max2468tw@gmail.com>
2017-11-26 04:48:54 +0800

    complete hw2 kdtree

--- hw2/kdtree.py
+++ hw2/kdtree.py
@@ -1,19 +1,19 @@
 import numpy as np
+from math import sqrt
 import sys
 
-data = np.loadtxt('trai.csv', delimiter = ',', dtype={'names':('index','0','1','2','3','4','5','6','7','8','9','10'),'formats': ('i4','U15','f8','f8','f8','f8','f8','f8','f8','f8','f8','U5')})
-
 class Tree(object):
-    def __init__(self):
-        self.left = None
-        self.right = None
-        self.data = None
+	def __init__(self):
+		self.parent = None
+		self.left = None
+		self.right = None
+		self.data = None
 
 def normalize(data):
 	normalized_data = data
 	names = np.asarray(data.dtype.names)
 	for i in range(names.size-1):
-		if i != 0 and i != 1 and i!=names.size-1:
+		if i != 0 and i != 1 and i != names.size-1:
 			max = 0
 			min = sys.float_info.max
 			for j in range(data.size-1):
@@ -37,63 +37,170 @@ def choose_middle(data, attribute):
 		mid = (data.size + 1)/2
 	else:
 		mid = (data.size)/2
-	mid = int(mid)
-	return data[mid - 1]
+	mid = int(mid - 1)
+	return data[mid]
 
 def create_kdtree(data, attribute):
 	if data.size == 0:
 		return None
 	elif data.size == 1:
 		root = Tree()
-		root.data = data[0]
+		root.data = [data[0], attribute]
 		return root
 	else:		
 		best = choose_middle(data,attribute)
 		root = Tree()
-		root.data = best
+		root.data = [best,attribute]
 		data0 = data1 = np.array([],dtype = data.dtype)
 		for i in range(data.size):
-			if data[i]['index'] != best['index']:
+			if data[i] != best:
 				if data[i][attribute] < best[attribute]:
 					data0 = np.append(data0,data[i])
 				else:
 					data1 = np.append(data1,data[i])		
-		get_next_attribute(attribute)
+		attribute = get_next_attribute(attribute)
 		subtree = create_kdtree(data0, attribute)
 		root.left = subtree
+		if subtree != None:
+			subtree.parent = root
 		subtree = create_kdtree(data1, attribute)
 		root.right = subtree
+		if subtree != None:
+			subtree.parent = root
 	return root
-x = create_kdtree(data, '1')
 
-def descendTree(x, q, attribute):
+def d(q, x):
+	sum = 0
+	names = np.asarray(x.data[0].dtype.names)
+	for i in range(names.size-1):
+		if i != 0 and i != 1 and i != names.size-1:
+			sum = sum + (x.data[0][names[i]] - q[names[i]]) * (x.data[0][names[i]] - q[names[i]])
+	return sqrt(sum)
+
+def descendTree(x, q):
+	if x == None:
+		return None
 	if x.left == None and x.right == None:
-		return [x,attribute]
-	elif q[attribute] >= x.data[attribute]:
+		return x
+	elif q[x.data[1]] >= x.data[0][x.data[1]]:
 		if x.right != None:
-			get_next_attribute(attribute)
-			return descendTree(x.right, q, attribute)
+			return descendTree(x.right, q)
 		else:
-			return [x,attribute]
+			return descendTree(x.left, q)
 	else:
 		if x.left != None:
-			get_next_attribute(attribute)
-			return descendTree(x.left, q, attribute)
+			return descendTree(x.left, q)
 		else:
-			return [x,attribute]
-print (data[100])
-print (descendTree(x, data[100], '1'))
+			return descendTree(x.right, q)
+
+def parent(x):
+	parent = x.parent
+	if parent == None:
+		return None
+	if parent.left != None and x == parent.left:
+		parent.left = None
+	elif parent.right != None and x == parent.right:
+		parent.right = None
+	return parent
+
+def boundaryDist(q, x):
+	return q[x.data[1]] - x.data[0][x.data[1]]
+
 def find_the_nearest(q, r):
 	t = None
-	d_t = 10000000000000
-	x = descendTree(r,q,'1')
+	d_t = 1000000000
+	x = descendTree(r,q)
 	while x != None:
 		if d(q,x) < d_t:
 			t = x
 			d_t = d(q,x)
-		if boundaryDist(q,x) < d_t:
-			x = descendTree(x,q,'0')
+		if x.left != None and x.right != None and boundaryDist(q,x) < d_t:
+			x = descendTree(x,q)
 		else:
 			x = parent(x)
 	return t
 
+def check_result(x):
+	if x == "cp":
+		return 0
+	elif x == "im":
+		return 1
+	elif x == "pp":
+		return 2
+	elif x == "imU":
+		return 3
+	elif x == "om":
+		return 4
+	elif x == "omL":
+		return 5
+	elif x == "imL":
+		return 6
+	elif x == "imS":
+		return 7
+
+def KNN(trainning_data, testing_data, k, list):
+	tmp_data = trainning_data
+	trace = [0,0,0,0,0,0,0,0]
+	for i in range(k):
+		tree = create_kdtree(tmp_data, '1')
+		x = find_the_nearest(testing_data,tree)	
+		list = np.append(list, x.data[0]['index'])
+		trace[check_result(x.data[0]['10'])] = trace[check_result(x.data[0]['10'])] + 1
+		for j in range(tmp_data.size):
+			if tmp_data[j] == x.data[0]:
+				tmp_data = np.delete(tmp_data,j)
+				break
+	max = 0
+	max_id = 0
+	for i in range(8):
+		if trace[i] > max:
+			max = trace[i]
+			max_id = i
+	return [max_id, list]	 
+
+def main(k):
+	trainning_data = np.loadtxt('trai.csv', delimiter = ',', dtype={'names':('index','0','1','2','3','4','5','6','7','8','9','10'),'formats': ('i4','U15','f8','f8','f8','f8','f8','f8','f8','f8','f8','U5')})
+	testing_data = np.loadtxt('test.csv', delimiter = ',', dtype={'names':('index','0','1','2','3','4','5','6','7','8','9','10'),'formats': ('i4','U15','f8','f8','f8','f8','f8','f8','f8','f8','f8','U5')})
+	t = f = 0
+	list = list1 = list2 = list3 = np.array([])
+	print('KNN accuracy:', end = " ")
+	for i in range(testing_data.size):
+		if i == 0:
+			x = KNN(trainning_data,testing_data[i],k, list1)
+			if check_result(testing_data[i]['10']) == x[0]:
+				list1 = x[1]
+				t = t + 1
+			else:
+				list1 = x[1]
+				f = f + 1
+		elif i == 1:
+			x = KNN(trainning_data,testing_data[i],k, list2)
+			if check_result(testing_data[i]['10']) == x[0]:
+				list2 = x[1]
+				t = t + 1
+			else:
+				list2 = x[1]
+				f = f + 1
+		elif i == 2:
+			x = KNN(trainning_data,testing_data[i],k, list3)
+			if check_result(testing_data[i]['10']) == x[0]:
+				list3 = x[1]
+				t = t + 1
+			else:
+				list3 = x[1]
+				f = f + 1
+		else:		
+			if check_result(testing_data[i]['10']) == KNN(trainning_data,testing_data[i],k, list)[0]:
+				t = t + 1
+			else:
+				f = f + 1
+	print(t/(t+f))
+	print(list1)
+	print(list2)
+	print(list3)
+	print()
+	return
+main(1)
+main(5)
+main(10)
+main(100)
